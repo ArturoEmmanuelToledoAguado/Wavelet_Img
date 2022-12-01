6332e60c106c872b953f18e1e3bdb69178c10ac9
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-30 20:37:04 -0600

    2 Iteraciones

--- Wavelet.m
+++ Wavelet.m
@@ -10,8 +10,13 @@ M=imread("IMG\M.jpg");
 P=rgb2gray(P);
 M=rgb2gray(M);
 
+figure
+imshow(P)
+
 %Iteraciones
 [Ca,Ch,Cv,Cd]=dwt2(P,'haar');
+[Ca2,Ch2,Cv2,Cd2]=dwt2(Cd,'haar');
+
 figure
 subplot(2,2,1)
 imshow(Ca)
@@ -21,3 +26,11 @@ subplot(2,2,3)
 imshow(Cv)
 subplot(2,2,4)
 imshow(Cd)
+
+%Rearmado
+[f,c]=size(Cd2);
+M=imresize(M,[f c]);
+Imrec2=idwt2(Ca2,Ch2,Cv2,M,'haar');
+Imrec=idwt2(Ca,Ch,Cv,Imrec2,'haar');
+figure
+imshow(Imrec)
