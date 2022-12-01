0a3b6edafbceddfb413cce498c85237563f64b2b
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-30 19:57:51 -0600

    Prototipo 1

--- Wavelet.m
+++ Wavelet.m
@@ -5,3 +5,19 @@ close all;
 %Abrir imagenes
 P=imread("IMG\P.jpg");
 M=imread("IMG\M.jpg");
+
+%Escala de grises
+P=rgb2gray(P);
+M=rgb2gray(M);
+
+%Iteraciones
+[Ca,Ch,Cv,Cd]=dwt2(P,'haar');
+figure
+subplot(2,2,1)
+imshow(Ca)
+subplot(2,2,2)
+imshow(Ch)
+subplot(2,2,3)
+imshow(Cv)
+subplot(2,2,4)
+imshow(Cd)
