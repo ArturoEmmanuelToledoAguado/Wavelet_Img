c11084307e393ca93411cd2123953a2e43ef5535
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-12-13 19:26:40 -0600

    Fin

--- Wavelet.m
+++ Wavelet.m
@@ -2,29 +2,31 @@ clc;
 clear all;
 close all;
 
-%Abrir imagenes
-P=imread("IMG\P.jpg");
-M=imread("IMG\M.jpg");
-
 wav=["haar","coif1","fk4","dmey","bior1.1","rbio1.1"];
-
-%Escala de grises
-P=rgb2gray(P);
-M=rgb2gray(M);
-
-figure('Name','Imagenes')
-imshowpair(P,M,"montage")
-title('Portadora         Mensaje')
+P=["IMG/P.jpg","IMG/P1.jpg","IMG/P2.jpg","IMG/P3.jpg","IMG/P4.jpg","IMG/P5.jpg"];
+M=["IMG/M.jpg","IMG/M1.jpg","IMG/M2.jpg","IMG/M3.jpg","IMG/M4.jpg","IMG/M5.jpg"];
 
 for i=1:6
+    %Abrir imagenes
+    Por=imread(P(i));
+    Men=imread(M(i));
+
+    %Escala de grises
+    Por=rgb2gray(Por);
+    Men=rgb2gray(Men);
+
+    figure('Name','Imagenes')
+    imshowpair(Por,Men,"montage")
+    title('Portadora         Mensaje')
+
     %Iteraciones
-    [Ca,Ch,Cv,Cd]=dwt2(P,wav(i));
+    [Ca,Ch,Cv,Cd]=dwt2(Por,wav(i));
     [Ca2,Ch2,Cv2,Cd2]=dwt2(Cd,wav(i));
     [Ca3,Ch3,Cv3,Cd3]=dwt2(Cd2,wav(i));
     [Ca4,Ch4,Cv4,Cd4]=dwt2(Cd3,wav(i));
     [Ca5,Ch5,Cv5,Cd5]=dwt2(Cd4,wav(i));
 
-    figure
+    figure('Name','Wavelet')
     sgtitle(wav(i))
     subplot(2,2,1)
     imshow(Ca)
@@ -37,26 +39,18 @@ for i=1:6
 
     %Rearmado
     [f,c,d]=size(Cd5);
-    M=imresize(M,[f c]);
-    Imrec5=idwt2(Ca5,Ch5,Cv5,M,wav(i));
-    if size(Imrec5)~= size(Cd4)
-        Imrec5=imresize(Imrec5,size(Cd4));
-    end
+    Men=imresize(Men,[f c]);
+    Imrec5=idwt2(Ca5,Ch5,Cv5,Men,wav(i));
+    Imrec5=imresize(Imrec5,size(Cd4));
     Imrec4=idwt2(Ca4,Ch4,Cv4,Imrec5,wav(i));
-    if size(Imrec4)~= size(Cd3)
-        Imrec4=imresize(Imrec4,size(Cd3));
-    end
+    Imrec4=imresize(Imrec4,size(Cd3));
     Imrec3=idwt2(Ca3,Ch3,Cv3,Imrec4,wav(i));
-    if size(Imrec3)~= size(Cd2)
-        Imrec3=imresize(Imrec3,size(Cd2));
-    end
+    Imrec3=imresize(Imrec3,size(Cd2));
     Imrec2=idwt2(Ca2,Ch2,Cv2,Imrec3,wav(i));
-    if size(Imrec2)~= size(Cd)
-        Imrec2=imresize(Imrec2,size(Cd));
-    end
+    Imrec2=imresize(Imrec2,size(Cd));
     Imrec=idwt2(Ca,Ch,Cv,Imrec2,wav(i));
     Imrec=uint8(Imrec);
-    figure
+    figure('Name','Resultado')
     imshow(Imrec)
     title('Imagenes en una')
 end
